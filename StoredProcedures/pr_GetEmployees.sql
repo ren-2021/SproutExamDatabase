aff07cf904dce3233a9b16752601c3a355d5e63d
Rendel Sevilla <ifl.rendelsevilla@gmail.com>
2024-05-20 04:33:05 +0800

    Update

--- StoredProcedures/pr_GetEmployees.sql
+++ StoredProcedures/pr_GetEmployees.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER PROC pr_GetEmployees
 AS
 BEGIN
-   SELECT Id, FullName, CONVERT(VARCHAR(20), CAST(Birthdate AS DATE)) AS Birthdate, TIN, EmployeeTypeId as [TypeId] FROM Employee WHERE IsDeleted <> 0
+   SELECT Id, FullName, CONVERT(VARCHAR(20), CAST(Birthdate AS DATE)) AS Birthdate, TIN, EmployeeTypeId as [TypeId] FROM Employee WHERE IsDeleted <> 1
 END
